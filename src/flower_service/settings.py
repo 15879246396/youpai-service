c674e94979234afa83794a3ca9f510e1c764ce70
Xiang bo <1548862429@qq.com>
2019-10-24 09:15:18 +0800

    m

--- src/flower_service/settings.py
+++ src/flower_service/settings.py
@@ -254,11 +254,11 @@ LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
-USE_I18N = True
+USE_I18N = False
 
-USE_L10N = True
+USE_L10N = False
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
